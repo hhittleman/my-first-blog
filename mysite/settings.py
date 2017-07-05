77adb0b1de22f28ca59de3b92178c0673c7aba10
hhittleman <hannah.hittleman@gmail.com>
2017-07-05 13:37:57 -0700

    my first updated django

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%=gbt(3d_ao-dt7%s+_*!m=1!h5$8$jkj)k#d9=0j*k-@3j&w&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'hhittleman.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
